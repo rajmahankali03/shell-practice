83b23e82194882f28acdc851002585a509d0e760
RajMahankali <rajmahankali@gmail.com>
2025-07-27 16:42:34 +0530

    01-hello-world started

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -2,7 +2,7 @@
 
 userid=$(id -u)
 
-if [ $userid -ne 1001 ];  # Check if the user ID is not equal to 0 (root user)]
+if [ $userid -ne 0 ];  # Check if the user ID is not equal to 0 (root user)]
 then
     echo "You are not root user, please run as root"
     exit 1
