faaf912adda6ff3d497124ab3ad142d0a748ca44
RajMahankali <rajmahankali@gmail.com>
2025-07-27 16:44:53 +0530

    01-hello-world started

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -4,8 +4,8 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ];  # Check if the user ID is not equal to 0 (root user)]
 then
-    echo "You are not root user, please run as root"
-    exit 1
+    echo "Error: You are not root user, please run as root"
+    exit 1 # Exit the script with an error code if not root
     else
     echo "You are root user, proceeding with installation"
 fi
