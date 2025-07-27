98c05c4cd7f76d5f818383de7516531bdaa200a1
RajMahankali <rajmahankali@gmail.com>
2025-07-27 20:07:35 +0530

    01-hello-world started

--- 15-for-loop.sh
+++ 15-for-loop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script demonstrates a for loop in bash
-for i in {1..100   # Loop from 1 to 100
+for i in {1..100}   # Loop from 1 to 100
 do 
 echo "Number: $i"  # Print the current number
 done
 echo "Loop completed"  # Print a message after the loop ends
-}
+# This script will print numbers from 1 to 100
