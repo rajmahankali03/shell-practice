c9d37604be809ff5e402ead852299c2b9e21610d
RajMahankali <rajmahankali@gmail.com>
2025-07-27 14:08:02 +0530

    02-conversation.sh

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -4,5 +4,5 @@ p1=$1 #no space bfre and after =
 p2=$2
 echo "$p1: Hi $p2, How are u"
 echo "$p2:Iam good!$p1!"
-echo "$p1: Tell me about today"
+echo "$p1: Tell me about today...."
 echo "$p2:Nothing special. Tell me about your day"
