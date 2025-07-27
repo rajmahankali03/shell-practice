84bed9fa169654a560f7f7a52817fc1d17815ccd
RajMahankali <rajmahankali@gmail.com>
2025-07-27 16:00:51 +0530

    01-hello-world started

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -5,6 +5,15 @@ n2=200
 
 timestamp=$(date)
 echo "Script executed at $timestamp"
-sum=$(($n1+$n2));
+
 echo "sum of 2 numbers is = $sum"
 
+echo "if you want all variables passed to script: $@"
+echo "number of variables passed: $#"
+echo "script name: $0"
+echo "present working directory: $PWD"
+echo "home directory of the user who is running the script: $HOME"
+echo "which user is running the script: $USER"
+echo "PID of the current script: $$"
+echo "sleep 10 &"
+echo "PID of the last command running in background: $!"
