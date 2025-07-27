03d083aa711697965623d9f722ac53df5705ee1d
RajMahankali <rajmahankali@gmail.com>
2025-07-27 15:34:28 +0530

    01-hello-world started

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-p1=raj
-p2=ram
+
+p1=Raj #no space bfre and after =
+p2=Ram
+echo "$p1: Hi $p2, How are u"
+echo "$p2:Iam good!$p1!"
+echo "$p1: Tell me about today...."
+echo "$p2:Nothing special. Tell me about your day"
