7b54b8dda8022ea08529ddff52e52667e1ecb51b
RajMahankali <rajmahankali@gmail.com>
2025-07-27 12:38:45 +0530

    02-conversation.sh

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Self: How are myself"
-echo "Raj:Iam not OK and I need your help!!"
-echo "Self: Sure I am there here for you, tell me what you want from me"
-echo "Raj:Nothing be with me and make me strong and feel secure and get me lessing from God to overcome my issue."
+echo "Ram: How are u"
+echo "Raj:Iam good!!"
+echo "Ram: Tell me about today"
+echo "Raj:Nothing special."
