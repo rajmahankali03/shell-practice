cefbcb4008d202683393d5be58159fcda63ec121
RajMahankali <rajmahankali@gmail.com>
2025-07-27 15:35:59 +0530

    01-hello-world started

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+p1=$1 #no space bfre and after =
+p2=$2
+echo "$p1: Hi $p2, How are u"
+echo "$p2:Iam good!$p1!"
+echo "$p1: Tell me about today...."
+echo "$p2:Nothing special. Tell me about your day"
