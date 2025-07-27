93dbc1966ed3b70f496dcfb62329551ba14299d0
RajMahankali <rajmahankali@gmail.com>
2025-07-27 15:52:25 +0530

    01-hello-world started

--- 07-array.sh
+++ 07-array.sh
@@ -2,7 +2,7 @@
 Movies=("Indra" "Tagoor" "SVSK")
 echo "Movie no one is ${Movies[0]}"
 echo "Movie no one is ${Movies[2]}"
-echo "Movie no one is ${Movies[1]}"
+echo "Movie no one is ${Movies[4]}" # index which is not avaialble 
 
-echo "Movie names ${Movies[@]}"
+echo "Movie names ${Movies[@]}" #to print all in an array
 
