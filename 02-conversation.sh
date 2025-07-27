77cb32c7d80faa913292e4df7c441512e3b907d3
RajMahankali <rajmahankali@gmail.com>
2025-07-27 11:50:00 +0530

    01-hello-world started

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Self: How are myself"
+echo "Raj:Iam not OK and I need your help!!"
+echo "Self: Sure I am there here for you, tell me what you want from me"
+echo "Raj:Nothing be with me and make me strong and feel secure and get me lessing from God to overcome my issue."
