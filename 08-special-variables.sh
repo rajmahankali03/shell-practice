9b22bacef327839c6b8e71b4dbd442466bef7617
RajMahankali <rajmahankali@gmail.com>
2025-07-27 16:08:11 +0530

    01-hello-world started

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,5 +7,5 @@ echo "present working directory: $PWD"
 echo "home directory of the user who is running the script: $HOME"
 echo "which user is running the script: $USER"
 echo "PID of the current script: $$"
-echo "sleep 10 &"
+sleep 10 &
 echo "PID of the last command running in background: $!"
