0dae844e2db2bb87cf8eb2759ebc37d951fa1ca7
RajMahankali <rajmahankali@gmail.com>
2025-07-27 19:03:33 +0530

    01-hello-world started

--- 12-colors.sh
+++ 12-colors.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
  echo -e "\e[35m hello colors"
+    echo -e "\e[31m red color"
+    echo -e "\e[32m green color"
+    echo -e "\e[33m yellow color"
+    echo -e "\e[34m blue color"
+    echo -e "\e[36m cyan color"
+    echo -e "\e[37m white color"            
+    echo -e "\e[0m default color"
+    echo -e "\e[1;31m bold red color"
+    echo -e "\e[1;32m bold green color"
+    echo -e "\e[1;33m bold yellow color"
+    echo -e "\e[1;34m bold blue color"
+    echo -e "\e[1;36m bold cyan color"
+    echo -e "\e[1;37m bold white color"
+
+
+
