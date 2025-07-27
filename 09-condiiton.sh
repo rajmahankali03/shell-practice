95eb3afe041deede8bea77486382ad43f0002607
RajMahankali <rajmahankali@gmail.com>
2025-07-27 16:14:02 +0530

    01-hello-world started

---
+++ 09-condiiton.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+number=$1
+
+echo "enter number " $number= -s
+if [$number <10]{
+
+}else if [$number <20]{
+  echo "number is less than 20"
+}else if [$number <30]{
+    echo "Enter Number <30 " -s 
+}fi
