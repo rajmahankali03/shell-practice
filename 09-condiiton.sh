64b60e140270ddf6a084d2a8ca8961486cec2b23
RajMahankali <rajmahankali@gmail.com>
2025-07-27 16:17:27 +0530

    01-hello-world started

--- 09-condiiton.sh
+++ 09-condiiton.sh
@@ -2,11 +2,12 @@
 
 number=$1
 
-echo "enter number " $number= -s
-if [$number <10]{
+echo "enter number "
+number = read -s
 
-}else if [$number <20]{
-  echo "number is less than 20"
-}else if [$number <30]{
-    echo "Enter Number <30 " -s 
-}fi
+if [$number -lt 10]
+then
+echo"Given $number is less than 10"
+else [$number -gt 10]
+fi
+
