2ad6fc2703854d67691ff23a9cb4f61f527f4ff5
RajMahankali <rajmahankali@gmail.com>
2025-07-27 13:00:35 +0530

    01-hello-world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,7 +2,7 @@
 
 p1=Ram p2=Raj
 
-echo "$p1: How are u"
-echo "$p2:Iam good!!"
+echo "$p1: Hi $p2, How are u"
+echo "$p2:Iam good!$p1!"
 echo "$p1: Tell me about today"
 echo "$p2:Nothing special. Tell me about your day"
