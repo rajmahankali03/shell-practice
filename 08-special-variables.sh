8c74bb287acc60aa339390d8de5383baa87534f6
RajMahankali <rajmahankali@gmail.com>
2025-07-27 17:15:47 +0530

    01-hello-world started

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -7,5 +7,6 @@ echo "present working directory: $PWD"
 echo "home directory of the user who is running the script: $HOME"
 echo "which user is running the script: $USER"
 echo "PID of the current script: $$"
+echo "$?" # This will show the exit status of the last command executed
 sleep 10 &
 echo "PID of the last command running in background: $!"
